65fc294c13b0ecfa0dd0ca855c786b54f4a3fafa
sanjai0412 <sanjaikanth44@gmail.com>
2026-08-26 14:14:46 +0530

    fix: update cors filter

--- src/main/java/org/example/config/CorsFilter.java
+++ src/main/java/org/example/config/CorsFilter.java
@@ -9,32 +9,64 @@ import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
 import java.io.IOException;
+import java.util.Set;
 
 @Provider
 @PreMatching
 public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
+    private static final Set<String> ALLOWED_ORIGINS = Set.of(
+            "http://localhost:5173",
+            "https://pingx-sanjaii04.vercel.app"
+    );
+
+    private boolean isAllowedOrigin(String origin) {
+        return origin != null && ALLOWED_ORIGINS.contains(origin);
+    }
+
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        // If it's a preflight request, abort with 200 OK
-        if (isPreflightRequest(requestContext)) {
-            requestContext.abortWith(Response.ok().build());
+
+        String origin = requestContext.getHeaderString("Origin");
+
+        if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod())
+                && isAllowedOrigin(origin)) {
+
+            requestContext.abortWith(
+                    Response.ok()
+                            .header("Access-Control-Allow-Origin", origin)
+                            .header("Access-Control-Allow-Credentials", "true")
+                            .header(
+                                    "Access-Control-Allow-Methods",
+                                    "GET, POST, PUT, DELETE, OPTIONS, HEAD"
+                            )
+                            .header(
+                                    "Access-Control-Allow-Headers",
+                                    "Origin, Content-Type, Accept, Authorization"
+                            )
+                            .build()
+            );
         }
     }
 
-    private boolean isPreflightRequest(ContainerRequestContext requestContext) {
-        return "OPTIONS".equalsIgnoreCase(requestContext.getMethod()) &&
-                requestContext.getHeaderString("Origin") != null;
-    }
-
     @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+    public void filter(
+            ContainerRequestContext requestContext,
+            ContainerResponseContext responseContext
+    ) throws IOException {
+
         String origin = requestContext.getHeaderString("Origin");
-        if (origin != null) {
-            responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", origin);
-            responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
-            responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-            responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
+
+        if (isAllowedOrigin(origin)) {
+            responseContext.getHeaders().putSingle(
+                    "Access-Control-Allow-Origin",
+                    origin
+            );
+
+            responseContext.getHeaders().putSingle(
+                    "Access-Control-Allow-Credentials",
+                    "true"
+            );
         }
     }
 }
